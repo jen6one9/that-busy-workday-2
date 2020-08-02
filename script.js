1436ab503c81d59e52ed9fb9ea81a154f6a71542
Jeneth Diesta <jen6one9@gmail.com>
2020-08-01 23:26:10 -0700

    updated time blocks and finally displayed the date

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-// function myCalendar() {
-//     $(document).ready(function () {
-//         let momentVar = moment().format('dddd, MMMM DD, YYYY');
-
-//         let workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
- 
-// function currentDay(){
-//     let currentDay = $("#currentDay").text(momentVar);
-//         return currentDay;
-// }
-
 function displayDate() {
-    const currentDate = moment().format('dddd, MMMM DD, YYYY')
+    const currentDate = moment().format('dddd, MMMM, YYYY')
     const currentDateEl = $("#currentDay");
     currentDateEl.text(currentDate)
-  }
-  
-  
-  
-  $(document).ready(function() {
+}
+
+$(document).ready(function () {
     displayDate()
-  })
+})
+
+//Show the timeblocks for a standard workday
+let workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
+//For loop to do this action repeatedly
+
+
+
+let inputId = document.getElementsByClassName("userInput");
+let length = inputId.length;
+for (i = 0; i < length; i++) {
+    inputId[i].id = "inputField" + (i + 1);
+
+}
+;
