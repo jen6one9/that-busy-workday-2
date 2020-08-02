91d987663211ae602dc0dda56457cfe34c001e67
Jeneth Diesta <jen6one9@gmail.com>
2020-08-01 21:40:21 -0700

    completed the fix to display the date on the UI

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function myCalendar() {
-    $(document).ready(function () {
-        let momentVar = moment().format('dddd, MMMM DD, YYYY');
+// function myCalendar() {
+//     $(document).ready(function () {
+//         let momentVar = moment().format('dddd, MMMM DD, YYYY');
 
-        let workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+//         let workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
  
-function currentDay(){
-    let currentDay = $("#currentDay").text(momentVar);
-        return currentDay;
-}
+// function currentDay(){
+//     let currentDay = $("#currentDay").text(momentVar);
+//         return currentDay;
+// }
+
+function displayDate() {
+    const currentDate = moment().format('dddd, MMMM DD, YYYY')
+    const currentDateEl = $("#currentDay");
+    currentDateEl.text(currentDate)
+  }
+  
+  
+  
+  $(document).ready(function() {
+    displayDate()
+  })
