0068927060139af275fd8744ff5fc41d92789029
Jeneth Diesta <jen6one9@gmail.com>
2020-08-02 20:06:25 -0700

    updated the spacing of the time block rows

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ function displayDate() {
     const currentDate = moment().format('dddd, MMMM DD, YYYY')
     const currentDateEl = $("#currentDay");
     currentDateEl.text(currentDate)
-    console.log("currentDate",currentDate)
+    console.log("currentDate", currentDate)
 }
 
 $(document).ready(function () {
     displayDate()
     var currentHour = moment().hour();
     console.log(currentHour);
-    $(".saveBtn").on("click",function(event) {
+    $(".saveBtn").on("click", function (event) {
         event.preventDefault();
         let inputField = $(this).siblings("input").val()
         let hourblock = $(this).siblings("input").attr("id")
         console.log(inputField, hourblock)
-        localStorage.setItem(hourblock,inputField)
-        
-    })   
+        localStorage.setItem(hourblock, inputField)
+
+    })
     //loop through the time blocks and obtain the values from local storage and display on HTML page. 
-    for (let i=8; i <= 17; i++){
-        $("#"+i).val(localStorage.getItem(i))
-        if (i < currentHour){
-            $("#"+i).addClass("bg-secondary")
+    for (let i = 8; i <= 17; i++) {
+        $("#" + i).val(localStorage.getItem(i))
+        if (i < currentHour) {
+            $("#" + i).addClass("bg-secondary")
         }
-        else if (i == currentHour){
-            $("#"+i).addClass("bg-info")
+        else if (i == currentHour) {
+            $("#" + i).addClass("bg-info")
         }
         else {
-            $("#"+i).addClass("bg-dark")
+            $("#" + i).addClass("bg-dark")
         }
     }
 })
